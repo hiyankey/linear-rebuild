f2d4ecc258a593ff752ef0384095c0eab4d06152
Emmanuel Yankey <hiyankey@gmail.com>
2025-04-09 14:13:42 +0000

    refactor menu

--- app/ui/header.tsx
+++ app/ui/header.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { cx } from '@/lib/utils';
 import Link from 'next/link';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Button } from './button';
 import Container from './container';
 import { Hamburger } from './icons/hamburger';
@@ -18,6 +18,23 @@ const navLinks = [
 ];
 export function Header() {
   const [menuIsOpen, setMenuIsOpen] = useState(false);
+
+  useEffect(() => {
+    const closeMenu = () => setMenuIsOpen(false);
+    window.addEventListener('orientationchange', () => closeMenu());
+    window.addEventListener('resize', () => closeMenu());
+
+    () => {
+      window.removeEventListener('orientationchange', () => closeMenu());
+      window.removeEventListener('resize', () => closeMenu());
+    };
+  }, []);
+  useEffect(() => {
+    const html = document.querySelector('html');
+    if (html) {
+      html?.classList.toggle('overflow-hidden', menuIsOpen);
+    }
+  }, [menuIsOpen]);
   return (
     <header className="fixed top-0 left-0 h-[var(--header-height)] w-full border-white/8 border-b bg-transparent backdrop-blur-sm">
       <Container className="flex h-full md:items-center">
@@ -32,15 +49,18 @@ export function Header() {
         >
           <nav
             className={cx(
-              'fixed top-[var(--header-height)] left-0 h-[calc(100vh-var(--header-height))] w-full bg-background transition-opacity duration-500 ease-in md:relative md:top-0 md:block md:h-full md:w-auto md:bg-transparent md:opacity-100',
-              menuIsOpen ? 'opacity-100' : 'opacity-0'
+              'fixed top-[var(--header-height)] left-0 h-[calc(100vh-var(--header-height))] w-full bg-background transition-opacity duration-500 ease-in md:relative md:top-0 md:block md:h-full md:w-auto md:translate-x-0 md:bg-transparent md:opacity-100',
+              menuIsOpen
+                ? 'translate-x-0 opacity-100'
+                : 'translate-x-[-100vw] opacity-0'
             )}
           >
             <ul
               className={cx(
                 'flex h-full flex-col md:flex-row md:items-center',
-                '[&_li]:h-auto md:[&_li]:nth-[3]:hidden md:[&_li]:nth-[4]:hidden md:[&_li]:nth-[5]:hidden lg:[&_li]:nth-[3]:block lg:[&_li]:nth-[4]:block lg:[&_li]:nth-[5]:block',
-                'px-3.5 text-lg [&_a:hover]:text-white/70 [&_a]:flex [&_a]:h-[var(--header-height)] [&_a]:items-center [&_a]:transition-colors md:[&_a]:ml-6 md:[&_a]:inline md:[&_a]:h-auto md:[&_a]:px-0 md:[&_a]:text-sm'
+                '[&_li]:h-auto [&_li]:transition-transform md:[&_li]:nth-[3]:hidden md:[&_li]:nth-[4]:hidden md:[&_li]:nth-[5]:hidden md:[&_li]:translate-y-0 md:[&_li]:transition-none lg:[&_li]:nth-[3]:block lg:[&_li]:nth-[4]:block lg:[&_li]:nth-[5]:block',
+                'px-3.5 text-lg [&_a:hover]:text-white/70 [&_a]:flex [&_a]:h-[var(--header-height)] [&_a]:items-center [&_a]:transition-colors md:[&_a]:ml-6 md:[&_a]:inline md:[&_a]:h-auto md:[&_a]:px-0 md:[&_a]:text-sm',
+                menuIsOpen ? '[&_li]:translate-y-0' : '[&_li]:translate-y-8'
               )}
             >
               {navLinks.map((link, index) => (
